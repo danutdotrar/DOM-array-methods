c7933be9990911b5b34808ce7edabe0747ca982b
danutdotrar <morarasu.danut@gmail.com>
2023-01-19 22:04:55 +0200

    Add calculate wealth function

--- app.js
+++ app.js
@@ -88,7 +88,7 @@ function showMillionaires() {
 
 // Calculate all users wealth
 function calculateWealth() {
-    const wealth = data.reduce((acc, user) => (acc += user.money), 0);
+    const wealth = data.reduce((acc, user) => (acc += +user.money), 0);
 
     const wealthEl = document.createElement('div');
     wealthEl.innerHTML = `<h3>Total Wealth: <strong>${formatMoney(wealth)}</strong></h3>`;
